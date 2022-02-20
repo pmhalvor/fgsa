0bce22d96d253de1da4b7161998359ed8a21c1f9
pmhalvor <pmchalvorsen@gmail.com>
2022-02-20 16:07:44 +0100

    cherry-pick changes from bert-target for clean dev on lstm

--- code/model.py
+++ code/model.py
@@ -168,8 +168,13 @@ class BertSimple(torch.nn.Module):
             predictions = self.predict(batch)
 
             # decode predictions and batch[2]
+<<<<<<< HEAD
             predict_decoded = decode_batch(predictions, mask=batch[1])
             true_decoded = decode_batch(batch[2], mask=batch[1])
+=======
+            predict_decoded = decode_batch(predictions, mask=batch[1], targets_only=self.targets_only)
+            true_decoded = decode_batch(batch[2], mask=batch[1], targets_only=self.targets_only)
+>>>>>>> f65bbde (fix yet another bug in model)
 
             annotations = ['expression', 'holder', 'polarity', 'target']
 
@@ -248,3 +253,256 @@ class BertSimple(torch.nn.Module):
                                 break
                         break
         return weights
+
+
+class BertLSTM(BertSimple):
+    """
+    Iteration on BertSimple where linear output is replaced with LSTM layer.
+    Hopefully, the LSTM will be better at decoding.
+    """
+    def __init__(
+        self, 
+        device,
+        num_labels,
+        bert_path="ltgoslo/norbert",  
+        bert_dropout=0.1,           # TODO tune
+        bert_finetune=True,         # TODO tune
+        ignore_id=-1,
+        lr=0.01,                    # TODO tune
+        lr_scheduler_factor=0.1,    # TODO tune
+        lr_scheduler_patience=2,    # TODO tune
+        num_layers=2,               # TODO tune
+        bidirectional=False,        # TODO tune
+        output_dim=5,  # target, holder, expression, polarity, intensity
+        tokenizer=None,
+    ):
+        """
+        Set up model specific architectures. 
+
+        """
+        super(BertSimple, self).__init__()
+
+        self.bidirectional = bidirectional
+        self.device = device
+        self.dropout = bert_dropout  # TODO potentially refactor name?
+        self.finetune = bert_finetune
+        self.learning_rate = lr
+        self.lr_scheduler_factor = lr_scheduler_factor
+        self.lr_scheduler_patience = lr_scheduler_patience
+        self.num_labels = num_labels
+        self.num_layers = num_layers
+        self.output_dim = output_dim
+        self.tokenizer = tokenizer
+
+        # initialize contextual embeddings
+        self.bert = BertModel.from_pretrained(bert_path)
+        self.lstm = torch.nn.LSTM(
+            input_size=786,  # output from BertModel is 786
+            hidden_size=786, # internal hidden states can be same as input for now
+            num_layers = self.num_layers,
+            dropout=self.dropout,  # same dropout as Bert for simplicity
+            bidirectional=bidirectional,
+        )
+        self.output = torch.nn.Linear(
+            in_features= 786*2 if self.bidirectional else 786,
+            out_features=num_labels 
+        )
+        self.bert.requires_grad = self.finetune
+        self.bert_dropout = torch.nn.Dropout(self.dropout)
+
+        # ensure everything is on specified device
+        self.bert = self.bert.to(self.device)
+        self.bert_dropout = self.bert_dropout.to(self.device)  # TODO is this needed?
+
+        # loss function
+        self.loss = torch.nn.CrossEntropyLoss(ignore_index=ignore_id)
+
+        # optimizer in model for sklearn-style fit() training
+        self.optimizer = torch.optim.Adam(
+            self.parameters(),
+            lr=self.learning_rate
+        )  # TODO test other optimizers?
+
+        # setting learning rate's scheduler
+        self._scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer=self.optimizer,
+            mode='min',
+            factor=self.lr_scheduler_factor,
+            patience=self.lr_scheduler_patience
+        )
+
+
+    def fit(self, train_loader, dev_loader=None, epochs=10):
+        for epoch in range(epochs):
+            self.train()
+            epoch_loss = 0
+            num_batches = 0
+
+            loader_iterator = tqdm(train_loader)
+            for b, batch in enumerate(train_loader):
+                self.train()        # turn off eval mode
+                self.zero_grad()    # clear updates from prev epoch
+
+                outputs = self.forward(batch)
+                
+                targets = batch[2]
+                
+                # apply loss
+                loss = self.backward(outputs.logits.permute(0, 2, 1), targets)
+                if b%13==0:
+                    logging.info("Epoch:{:3} Batch:{:3} Loss:{}".format(epoch, b, loss.item()))
+        
+            if dev_loader is not None:
+                self.evaluate(dev_loader)
+
+        logging.info("Fit complete.")
+
+    
+    def forward(self, batch):
+        """
+        One forward step of training for our model.
+
+        Parameters:
+            x: token ids for a batch
+        """
+        input_ids = batch[0].to(torch.device(self.device))
+        attention_mask = batch[1].to(torch.device(self.device))
+        labels = batch[2].to(torch.device(self.device))
+
+        return self.bert(
+            input_ids = input_ids,
+            attention_mask = attention_mask,
+            output_hidden_states = True,
+            labels = labels
+        )
+
+
+    def backward(self, outputs, targets):
+        """
+        Performs a backpropagation step computing the loss.
+        ______________________________________________________________
+        Parameters:
+        output:
+            The output after forward with shape (batch_size, num_classes).
+        target:
+            The real targets.
+        ______________________________________________________________
+        Returns:
+        loss: float
+            How close the estimate was to the gold standard.
+        """
+        # self.check_weights()
+
+        computed_loss = self.loss(
+            input=outputs.to(torch.device(self.device)),
+            target=targets.to(torch.device(self.device))  # FIXME where is this supposed to happen?
+            )
+
+        # calculating gradients
+        computed_loss.backward()
+
+        # self.check_weights()
+
+        # updating weights from the model by calling optimizer.step()
+        self.optimizer.step()
+
+        # self.check_weights()
+
+        return computed_loss
+
+
+    def evaluate(self, loader, verbose=False):
+        """
+        Returns the binary and proportional F1 scores of the model on the
+        examples passed via loader.
+
+        :param loader: torch.utils.data.DataLoader object with
+                            batch_size=1
+        """
+        f_absa_overall = 0
+
+        for b, batch in enumerate(loader):
+            predictions = self.predict(batch)
+
+            # decode predictions and batch[2]
+            predict_decoded = decode_batch(predictions, mask=batch[1], targets_only=self.targets_only)
+            true_decoded = decode_batch(batch[2], mask=batch[1], targets_only=self.targets_only)
+
+            annotations = ['expression', 'holder', 'polarity', 'target']
+
+            f_target, acc_polarity, f_polarity, f_absa = score(
+                true_aspect = true_decoded["targets"], 
+                predict_aspect = predict_decoded["targets"], 
+                true_sentiment = true_decoded["polarities"], 
+                predict_sentiment = predict_decoded["polarities"], 
+                train_op = False
+            )
+
+            f_expression, _, _, _ = score(
+                true_aspect = true_decoded["expressions"], 
+                predict_aspect = predict_decoded["expressions"], 
+                true_sentiment = true_decoded["polarities"], 
+                predict_sentiment = predict_decoded["polarities"], 
+                train_op = True
+            )
+
+            f_holder, _, _, _ = score(
+                true_aspect = true_decoded["holders"], 
+                predict_aspect = predict_decoded["holders"], 
+                true_sentiment = true_decoded["polarities"], 
+                predict_sentiment = predict_decoded["polarities"], 
+                train_op = True
+            )
+
+            if verbose:
+                logging.info("f_target: {}".format(f_target))
+                logging.info("f_expression: {}".format(f_expression))
+                logging.info("f_holder: {}".format(f_holder))
+                logging.info("acc_polarity: {}".format(acc_polarity))
+                logging.info("f_polarity: {}".format(f_polarity))
+
+            f_absa_overall = (f_absa + f_absa_overall)/2.
+            logging.info("dev batch: {:3}   overall f1 absa: {}".format(b, f_absa_overall))
+
+
+        return f_absa_overall, None
+
+
+    def predict(self, batch):
+        """
+        :param batch: tensor containing batch of dev/test data 
+        """
+        self.eval()
+
+        outputs = self.forward(batch)
+
+        self.predictions = outputs.logits.argmax(2)
+
+        return self.predictions
+        
+
+    def check_weights(self):
+        weights = None
+        for parent, module in self.bert.named_children():
+            if parent == "bert":
+                for child, mod in module.named_children():
+                    if child=="encoder":
+                        for layer, md in mod.named_children():
+                            if layer=="layer":
+                                for name, bert_layer in md.named_children():
+                                    if name=="0":
+                                        for att, wrapper in bert_layer.named_children():
+                                            if att=="attention":
+                                                for s, s_wrap in wrapper.named_children():
+                                                    if s=="self":
+                                                        for n, m in s_wrap.named_children():
+                                                            # logging.info("Name:{}  Module:{}  Weight:{}".format(n, m, m.weight))
+                                                            weights = m.weight
+                                                            break
+                                                        break
+                                                break
+                                        break
+                                break
+                        break
+        return weights
+
