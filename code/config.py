80e7970c56e0d38c7ca3b16d045b018d847c9cc1
pmhalvor <pmchalvorsen@gmail.com>
2022-02-22 00:27:41 +0100

    store logs to dirs by date

--- code/config.py
+++ code/config.py
@@ -1,26 +1,24 @@
 import logging
+import os 
+import datetime
 
 # TODO download from git automagically
 BERT_PATH = "ltgoslo/norbert"
 DATA_DIR = "/fp/homes01/u01/ec-pmhalvor/data/norec_fine/"  # TODO hide personal info
 
 
-# def log_test(level=logging.INFO, name='test'):
-#     """
-#     Expected to run by pytest files to  See pytest.ini for config. 
-#     Gets printed out in terminal/ slurm file. 
-#     """
-#     logger = logging.getLogger(__name__)
-#     logger.setLevel(level)
-#     logger.info('---------  new test: {} ---------'.format(name))
-#     return logger
-
-
 def log_template(level=logging.INFO, name='', job='train'):
     name = '-'+name if name != '' else name
 
+    today = datetime.date.today()
+    log_dir = '../log/' + today.strftime("%Y-%m-%d")
+    try:
+        os.mkdir(log_dir)
+    except:
+        print('Directory {log_dir} exists'.format(log_dir = log_dir))
+
     logging.basicConfig(
-        filename='../log/{job}{name}.log'.format(job=job, name=name),
+        filename='{log_dir}/{job}{name}.log'.format(log_dir=log_dir, job=job, name=name),
         level=level,
         format='%(asctime)s [%(levelname)8s] %(message)s (%(filename)s:%(lineno)s)'
     )
