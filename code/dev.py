d44b5c8a0bacce8263223489fe0a214d89db5a89
pmhalvor <pmchalvorsen@gmail.com>
2022-02-22 17:05:08 +0100

    fix parameter bug in dataset

--- code/dev.py
+++ code/dev.py
@@ -36,12 +36,14 @@ logging.info('Running on device {}'.format(DEVICE))
 # load train/dev/test data so every build has complete result set
 logging.info("Loading datasets..")
 train_dataset = Norec(
-    data_path=DATA_DIR + "train/", 
+    data_dir=DATA_DIR
+    partition="train/", 
     ignore_id=-1,
     proportion=proportion,
 )
 dev_dataset = Norec(
-    data_path=DATA_DIR + "dev/", 
+    data_dir=DATA_DIR
+    partition="dev/", 
     ignore_id=-1,
     proportion=proportion,
     tokenizer=train_dataset.tokenizer,
