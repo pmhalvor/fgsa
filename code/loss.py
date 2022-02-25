031f3219454449bc0ed9f90a90fa94d57a7d9d91
pmhalvor <pmchalvorsen@gmail.com>
2022-02-25 23:12:06 +0100

    fix f1_loss params

--- code/loss.py
+++ code/loss.py
@@ -1,6 +1,9 @@
 from torch import nn
 import torch 
 
+from utils import score
+
+
 def compute_per_channel_dice(input, target, epsilon=1e-6, weight=None):
     """
     Computes DiceCoefficient as defined in https://arxiv.org/abs/1606.04797 given  a multi channel input and target.
@@ -29,7 +32,7 @@ def compute_per_channel_dice(input, target, epsilon=1e-6, weight=None):
     return 2 * (intersect / union.clamp(min=epsilon))
 
 
-def f1_loss(y_true:torch.Tensor, y_pred:torch.Tensor, is_training=False) -> torch.Tensor:
+def f1_loss(target:torch.Tensor, input:torch.Tensor, is_training=False) -> torch.Tensor:
     '''Calculate F1 score. Can work with gpu tensors
     
     The original implmentation is written by Michal Haltuf on Kaggle.
@@ -46,14 +49,10 @@ def f1_loss(y_true:torch.Tensor, y_pred:torch.Tensor, is_training=False) -> torc
     - https://discuss.pytorch.org/t/calculating-precision-recall-and-f1-score-in-case-of-multi-label-classification/28265/6
     
     '''
-    assert y_true.ndim == 1
-    assert y_pred.ndim == 1 or y_pred.ndim == 2
+    y_pred = input.argmax(dim=1)
+    y_true = target
     
-    if y_pred.ndim == 2:
-        y_pred = y_pred.argmax(dim=1)
-        
-    
-    tp = (y_true * y_pred).sum().to(torch.float32)
+    tp = (y_true * y_pred).sum(-1).to(torch.float32)
     tn = ((1 - y_true) * (1 - y_pred)).sum().to(torch.float32)
     fp = ((1 - y_true) * y_pred).sum().to(torch.float32)
     fn = (y_true * (1 - y_pred)).sum().to(torch.float32)
@@ -63,7 +62,7 @@ def f1_loss(y_true:torch.Tensor, y_pred:torch.Tensor, is_training=False) -> torc
     precision = tp / (tp + fp + epsilon)
     recall = tp / (tp + fn + epsilon)
     
-    f1 = 2* (precision*recall) / (precision + recall + epsilon)
+    f1 = (2* (precision*recall) / (precision + recall + epsilon)).sum()
     f1.requires_grad = is_training
     return f1
 
@@ -162,3 +161,70 @@ class GeneralizedDiceLoss(_AbstractDiceLoss):
         denominator = (denominator * w_l).clamp(min=self.epsilon)
 
         return 2 * (intersect.sum() / denominator.sum())
+
+
+class F1_Loss(nn.Module):
+    '''Calculate F1 score. Can work with gpu tensors
+    
+    The original implmentation is written by Michal Haltuf on Kaggle.
+    
+    Returns
+    -------
+    torch.Tensor
+        `ndim` == 1. epsilon <= val <= 1
+    
+    Reference
+    ---------
+    - https://www.kaggle.com/rejpalcz/best-loss-function-for-f1-score-metric
+    - https://scikit-learn.org/stable/modules/generated/sklearn.metrics.f1_score.html#sklearn.metrics.f1_score
+    - https://discuss.pytorch.org/t/calculating-precision-recall-and-f1-score-in-case-of-multi-label-classification/28265/6
+    - http://www.ryanzhang.info/python/writing-your-own-loss-function-module-for-pytorch/
+    '''
+    def __init__(self, epsilon=1e-7):
+        super().__init__()
+        self.epsilon = epsilon
+        
+    def forward(self, y_pred, y_true,):
+        assert y_pred.ndim == 2
+        assert y_true.ndim == 1
+        y_true = F.one_hot(y_true, 2).to(torch.float32)
+        y_pred = F.softmax(y_pred, dim=1)
+        
+        tp = (y_true * y_pred).sum(dim=0).to(torch.float32)
+        tn = ((1 - y_true) * (1 - y_pred)).sum(dim=0).to(torch.float32)
+        fp = ((1 - y_true) * y_pred).sum(dim=0).to(torch.float32)
+        fn = (y_true * (1 - y_pred)).sum(dim=0).to(torch.float32)
+
+        precision = tp / (tp + fp + self.epsilon)
+        recall = tp / (tp + fn + self.epsilon)
+
+        f1 = 2* (precision*recall) / (precision + recall + self.epsilon)
+        f1 = f1.clamp(min=self.epsilon, max=1-self.epsilon)
+        return 1 - f1.mean()
+
+
+class RaclF1Loss(torch.nn.Module):
+    def __init__(self, epsilon=1e-7):
+        super().__init__()
+        self.epsilon = epsilon
+        
+    def forward(self, input, target):
+        raise NotImplementedError
+
+        target = F.one_hot(target, 2).to(torch.float32)
+        input = F.softmax(input, dim=1)
+        
+        tp = (target * input).sum(dim=0).to(torch.float32)
+        tn = ((1 - target) * (1 - input)).sum(dim=0).to(torch.float32)
+        fp = ((1 - target) * input).sum(dim=0).to(torch.float32)
+        fn = (target * (1 - input)).sum(dim=0).to(torch.float32)
+
+        precision = tp / (tp + fp + self.epsilon)
+        recall = tp / (tp + fn + self.epsilon)
+
+        f1 = 2* (precision*recall) / (precision + recall + self.epsilon)
+        f1 = f1.clamp(min=self.epsilon, max=1-self.epsilon)
+        return 1 - f1.mean()
+
+
+
