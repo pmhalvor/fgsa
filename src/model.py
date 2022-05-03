3266a0149f22c8ec7d1b5c2971fd767323c677c3
pmhalvor <pmchalvorsen@gmail.com>
2022-05-03 13:09:55 +0200

    test fgflex with no relations

--- src/model.py
+++ src/model.py
@@ -2089,6 +2089,8 @@ class FgFlex(BertHead):
         for stack in range(stack_count):
             relations[f"stack_{stack}"] = torch.nn.ModuleDict({})
             for rel in attention_relations:
+                if len(rel)==0:
+                    continue
                 if f"{rel[0]}_at_{rel[1]}" in relations:
                     continue
                 relations[f"stack_{stack}"][f"{rel[0]}_at_{rel[1]}"] = torch.nn.ModuleDict({
