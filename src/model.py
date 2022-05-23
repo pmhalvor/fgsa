6fdc0cbe28182bc180162d0bd4d4693e5f7bca7e
pmhalvor <pmchalvorsen@gmail.com>
2022-05-23 17:24:48 +0200

    more doc strings

--- src/model.py
+++ src/model.py
@@ -21,273 +21,6 @@ from utils import weighted_macro
 import config
 
 
-
-class BertSimple(torch.nn.Module):
-    """
-    Our first model built for fgsa on norec_fine. 
-    A very simplified multitask system, w/ shared Bert embeddings
-
-    Expects joint-labeled dataset ([B-Target, O, B-Expression, I-Expression]) 
-    Classifies num_labels outputs via BertForTokenClassification
-    """
-    def __init__(
-        self, 
-        device,
-        num_labels,
-        bert_path="ltgoslo/norbert",  
-        bert_dropout=0.1,           # TODO tune
-        bert_finetune=True,         # TODO tune
-        ignore_id=-1,
-        lr=0.01,                    # TODO tune
-        lr_scheduler_factor=0.1,    # TODO tune
-        lr_scheduler_patience=2,    # TODO tune
-        label_importance = 2,       # TODO remove or tune
-        output_dim=5,  # target, holder, expression, polarity, intensity
-        tokenizer=None,
-        targets_only = False
-    ):
-        """
-        Set up model specific architectures. 
-
-        """
-        super(BertSimple, self).__init__()
-
-        self.device = device
-        self.dropout = bert_dropout  # TODO potentially refactor name?
-        self.finetune = bert_finetune
-        self.learning_rate = lr
-        self.lr_scheduler_factor = lr_scheduler_factor
-        self.lr_scheduler_patience = lr_scheduler_patience
-        self.output_dim = output_dim
-        self.num_labels = num_labels
-        self.tokenizer = tokenizer
-        self.targets_only = targets_only
-
-        # initialize contextual embeddings
-        self.bert = BertForTokenClassification.from_pretrained(
-            bert_path, num_labels=self.num_labels
-        )
-        self.bert.requires_grad = self.finetune
-        self.bert_dropout = torch.nn.Dropout(self.dropout)
-
-        # ensure everything is on specified device
-        self.bert = self.bert.to(self.device)
-        self.bert_dropout = self.bert_dropout.to(self.device)  # TODO is this needed?
-
-        # loss function
-        w = 1. + label_importance*(self.num_labels - 1)
-        weight = [1/w] + [
-            label_importance/w for _ in range(self.num_labels-1)
-        ]  # want labels to be 2 as important as 0s
-        self.loss = torch.nn.CrossEntropyLoss(
-            ignore_index=ignore_id,
-            weight=torch.Tensor(weight),
-            )
-
-        # optimizer in model for sklearn-style fit() training
-        self.optimizer = torch.optim.Adam(
-            self.parameters(),
-            lr=self.learning_rate
-        )  # TODO test other optimizers?
-
-        # setting learning rate's scheduler
-        self._scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-            optimizer=self.optimizer,
-            mode='min',
-            factor=self.lr_scheduler_factor,
-            patience=self.lr_scheduler_patience
-        )
-
-
-    def fit(self, train_loader, dev_loader=None, epochs=10):
-        for epoch in range(epochs):
-            self.train()
-            epoch_loss = 0
-            num_batches = 0
-
-            loader_iterator = tqdm(train_loader)
-            for b, batch in enumerate(train_loader):
-                self.train()        # turn off eval mode
-                self.zero_grad()    # clear updates from prev epoch
-
-                outputs = self.forward(batch)
-                
-                targets = batch[2]
-
-                # apply loss
-                logits = outputs.logits.permute(0, 2, 1)
-                loss = self.backward(logits, targets)
-
-                if b%13==0:
-                    logging.info("Epoch:{:3} Batch:{:3} Loss:{}".format(epoch, b, loss.item()))
-        
-            if dev_loader is not None:
-                self.evaluate(dev_loader)
-
-        logging.info("Fit complete.")
-
-    
-    def forward(self, batch):
-        """
-        One forward step of training for our model.
-
-        Parameters:
-            x: token ids for a batch
-        """
-        input_ids = batch[0].to(torch.device(self.device))
-        attention_mask = batch[1].to(torch.device(self.device))
-        labels = batch[2].to(torch.device(self.device))
-
-        return self.bert(
-            input_ids = input_ids,
-            attention_mask = attention_mask,
-            output_hidden_states = True,
-            labels = labels
-        )
-
-
-    def backward(self, outputs, targets):
-        """
-        Performs a backpropagation step computing the loss.
-        ______________________________________________________________
-        Parameters:
-        output:
-            The output after forward with shape (batch_size, num_classes).
-        target:
-            The real targets.
-        ______________________________________________________________
-        Returns:
-        loss: float
-            How close the estimate was to the gold standard.
-        """
-        # self.check_weights()
-
-        computed_loss = self.loss(
-            input=outputs.to(torch.device(self.device)),
-            target=targets.to(torch.device(self.device))  # FIXME where is this supposed to happen?
-            )
-
-        # calculating gradients
-        computed_loss.backward()
-
-        # self.check_weights()
-
-        # updating weights from the model by calling optimizer.step()
-        self.optimizer.step()
-
-        # self.check_weights()
-
-        return computed_loss
-
-
-    def evaluate(self, loader, verbose=False):
-        """
-        Returns the binary and proportional F1 scores of the model on the
-        examples passed via loader.
-
-        :param loader: torch.utils.data.DataLoader object with
-                            batch_size=1
-        """
-        easy_total_over_batches = 0
-        hard_total_over_batches = 0
-
-
-        for b, batch in enumerate(loader):
-            predictions = self.predict(batch)
-
-            # decode predictions and batch[2]
-            predict_decoded = decode_batch(predictions, mask=batch[1], targets_only=self.targets_only)
-            true_decoded = decode_batch(batch[2], mask=batch[1], targets_only=self.targets_only)
-
-            annotations = ['expression', 'holder', 'polarity', 'target']
-
-            f_target, acc_polarity, f_polarity, f_absa = score(
-                true_aspect = true_decoded["targets"], 
-                predict_aspect = predict_decoded["targets"], 
-                true_sentiment = true_decoded["polarities"], 
-                predict_sentiment = predict_decoded["polarities"], 
-                train_op = False
-            )
-
-            ez = ez_score(batch[2], predictions, num_labels=self.num_labels)
-            logging.debug("label f1: {}".format(ez))
-
-            easy_total_over_batches += ez
-            hard_total_over_batches += f_absa
-
-            if not self.targets_only:
-                    
-                f_expression, _, _, _ = score(
-                    true_aspect = true_decoded["expressions"], 
-                    predict_aspect = predict_decoded["expressions"], 
-                    true_sentiment = true_decoded["polarities"], 
-                    predict_sentiment = predict_decoded["polarities"], 
-                    train_op = True
-                )
-
-                f_holder, _, _, _ = score(
-                    true_aspect = true_decoded["holders"], 
-                    predict_aspect = predict_decoded["holders"], 
-                    true_sentiment = true_decoded["polarities"], 
-                    predict_sentiment = predict_decoded["polarities"], 
-                    train_op = True
-                )
-
-            if verbose:
-                logging.info("f_target: {}".format(f_target))
-                logging.info("f_expression: {}".format(f_expression)) if not self.targets_only else None
-                logging.info("f_holder: {}".format(f_holder)) if not self.targets_only else None
-                logging.info("acc_polarity: {}".format(acc_polarity))
-                logging.info("f_polarity: {}".format(f_polarity))
-
-        easy_overall = easy_total_over_batches/len(loader)
-        hard_overall = hard_total_over_batches/len(loader)
-
-        logging.info("Easy overall: {easy}".format(easy=easy_overall))
-        logging.info("Hard overall: {hard}".format(hard=hard_overall))
-
-        return easy_overall, hard_overall
-
-
-    def predict(self, batch):
-        """
-        :param batch: tensor containing batch of dev/test data 
-        """
-        self.eval()
-
-        outputs = self.forward(batch)
-
-        self.predictions = outputs.logits.argmax(2)
-
-        return self.predictions
-        
-
-    def check_weights(self):
-        weights = None
-        for parent, module in self.bert.named_children():
-            if parent == "bert":
-                for child, mod in module.named_children():
-                    if child=="encoder":
-                        for layer, md in mod.named_children():
-                            if layer=="layer":
-                                for name, bert_layer in md.named_children():
-                                    if name=="0":
-                                        for att, wrapper in bert_layer.named_children():
-                                            if att=="attention":
-                                                for s, s_wrap in wrapper.named_children():
-                                                    if s=="self":
-                                                        for n, m in s_wrap.named_children():
-                                                            # logging.info("Name:{}  Module:{}  Weight:{}".format(n, m, m.weight))
-                                                            weights = m.weight
-                                                            break
-                                                        break
-                                                break
-                                        break
-                                break
-                        break
-        return weights
-
-
 class BertHead(torch.nn.Module):
     """
     Base flexible multitask learner model developed for this experiment.
@@ -297,7 +30,10 @@ class BertHead(torch.nn.Module):
     
     Prediction outputs dictionary of subtask label predictions.
 
-    Parameters: TODO write all parameters possible to use here..
+    Parameters: 
+        optimizer_name
+
+
     """
     def __init__(
         self, 
@@ -308,12 +44,12 @@ class BertHead(torch.nn.Module):
         bert_target=True,
         bert_path=config.BERT_PATH,  
         device="cuda" if torch.cuda.is_available() else "cpu",
-        dropout=0.1,                # TODO tune
+        dropout=0.1,            
         ignore_id=-1,
         loss_function="cross-entropy",  # cross-entropy, dice, mse, or iou 
-        lr=1e-7,                    # TODO tune
-        lr_scheduler_factor=0.1,    # TODO tune
-        lr_scheduler_patience=2,    # TODO tune
+        lr=1e-7,                
+        lr_scheduler_factor=0.1,
+        lr_scheduler_patience=2,
         subtasks = ["expression", "holder", "polarity", "target"], 
         tokenizer=None,             # need tokenizer used in preprocessing 
         
@@ -1120,6 +856,12 @@ class FgsaLSTM(BertHead):
     Increase complexity as simply as possible (testing flexability).
 
     Prediction outputs dictionary of subtask label predictions.
+
+    Parameters:
+        bidirectional (bool): whether lstms should flow in both directions or not (default = False)
+        hidden_size (int): number of nodes per lstm layer (default = 768)
+        num_layers (int): number of hidden layers in lstm components (default = 3)
+        <task>_lstm (int): which tasks to use an lstm on (default = True)
     """
 
     def init_components(self, subtasks):
@@ -1210,11 +952,16 @@ class IMN(BertHead):
     Similar to the original IMN structure, just built on a BertHead (in PyTorch).
 
     Parameters:
-        interactions (int, default=2)
-        expression_layers (int, default=2)
-        polarity_layers (int, default=2)
-        shared_layers (int, default=2)
-        target_layers (int, default=2)
+        cnn_dim (int): number of cnn outputs passed as hidden state (default = 768)
+        <task>_layers (int): task-specific layer counts (default = 1)
+        interactions (int): number of stacks/interactions model should have (default = 2)
+        find_scope (bool): ability to look for scope as an auxiliary task (default = False)
+        gold_transmission (bool): ability to use true labels in attention components for first few epochs (default = True)
+        scope_lr (float): set scope specific learning rate (default = self.learning_rate)
+        shared_lr (float): learning rate for shared components
+        query (str): ability to configure attention inputs for more model flexibility
+        key (str): ability to configure attention inputs for more model flexibility
+        value (str): ability to configure attention inputs for more model flexibility
     """
     def __init__(self, subtasks = ["target", "expression", "polarity"], **kwargs):
         # overwrite BertHead class default for subtasks
@@ -1392,7 +1139,6 @@ class IMN(BertHead):
         shared_layers = self.find("shared_layers", default=2)
         target_layers = self.find("target_layers", default=2)
         # attention params
-        queries = self.find("queries", default=batch[4])  # batch[4] = polarity
 
         input_ids = batch[0].to(torch.device(self.device))
         mask = batch[1].to(torch.device(self.device))
@@ -1647,7 +1393,11 @@ class RACL(IMN):
     Similar to original RACL architecture, just built on a BertHead (in PyTorch).
 
     Parameters:
-        TODO fill in parameters used in self.find()
+        cnn_dim (int): number of cnn outputs passed as hidden state (default = 768)
+        gold_transmission (bool): ability to use true labels in attention components for first few epochs (default = True)
+        kernel_size (int): kernel size for cnn components 
+        stack_count (int): number of stacks/interactions model should have (default = 1)
+        warm_up_constant (float): proportion of true labels to influence attention blocks, dependent on current epoch (default = 5)
     """
 
     def init_components(self, subtasks):
@@ -1961,17 +1711,17 @@ class FgFlex(BertHead):
     Allows experimenter to test different alterations of these setups.
 
     Parameters:
+        attention_relations (list(tuple(str, str))): subtasks desired to feed into attention components (default = None)
+        attn_lr (float): ability to specify specific learning rate for attention components (default = self.learning rate)
         cnn_dim (int): number of cnn outputs passed as hidden state (default = 768)
         expanding_cnn (int): ability to expand cnns even larger than normal. Not fully tested (default = None) 
-        split_cnn_kernels (list(int)): ability to use multiple kernel sizes for cnn filters (default = None)
+        gold_transmission (bool): ability to use true labels in attention components for first few epochs (default = True)
         kernel_size (int): kernel size for cnn components 
-        attn_lr (float): ability to specify specific learning rate for attention components (default = self.learning rate)
         shared_lr (float): ability to specify specific learning rate for shared components (default = self.learning_rate)
+        split_cnn_kernels (list(int)): ability to use multiple kernel sizes for cnn filters (default = None)
         stack_count (int): number of stacks/interactions model should have (default = 1)
-        attention_relations (list(tuple(str, str))): subtasks desired to feed into attention components (default = None)
         shared_layers (int): number of layers for shared component (default = 1)
         <task>_layers (int): task-specific layer counts (default = 1)
-        gold_transmission (bool): ability to use true labels in attention components for first few epochs (default = True)
         warm_up_constant (float): proportion of true labels to influence attention blocks, dependent on current epoch (default = 5)
     """
     def init_components(self, subtasks):
