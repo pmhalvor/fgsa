d25be0d29e6ce10054b84143fc8938ff97482021
pmhalvor <pmchalvorsen@gmail.com>
2022-05-23 15:16:01 +0200

    clean up single model train file

--- src/train.py
+++ src/train.py
@@ -6,49 +6,66 @@ import torch
 from config import DATA_DIR
 from config import log_template
 from dataset import Norec
-from dataset import NorecOneHot 
-from model import BertSimple
+from model import FgFlex
 from utils import pad
 
 
 ####################  config  ####################
-debug = True 
-epochs = 200
-label_importance = 10
-learning_rate = 1e-6
-proportion = 0.55
-load_checkpoint = False
+parameters = {
+    "name": "single-trained-FgFlex",
+    "device": "cuda" if torch.cuda.is_available() else "cpu",
 
-name = 'full-{percent}p-{epochs}e'.format(
-    percent=int(proportion*100),
-    epochs=epochs,
-)
-if debug:
-    name += "-debug"
-log_template(job="train", name=name)
+    "attention_relations": [
+        ["target", "expression"], 
+        ["target", "holder"], 
 
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-logging.info('Running on device {}'.format(DEVICE))
+        ["expression", "target"], 
+        ["expression", "holder"], 
+
+        ["holder", "target"], 
+        ["holder", "expression"], 
+
+        ["polarity", "target"], 
+        ["polarity", "holder"], 
+        ["polarity", "expression"]
+    ],     
+    "cnn_dim": 768,
+    "expanding_cnn": 0,
+    "gold_transmission": True,
+    "kernel_size": 5,
+    "learning_rate": 1e-5,
+    "stack_count": 2,
+
+    "shared_layers": 3,
+    "expression_layers": 3,
+    "holder_layers": 2,
+    "polarity_layers": 1,
+    "target_layers": 3,
+}
+
+logging.info('Running on device {}'.format(parameters["device"]))
 ###################################################
 
 
 # load train/dev/test data so every build has complete result set
 logging.info("Loading datasets..")
-train_dataset = NorecOneHot(
-    data_path=DATA_DIR + "train/", 
-    ignore_id=-1,
-    proportion=0.55,
+train_dataset = Norec(
+    data_dir=DATA_DIR,
+    proportion=1.,
+    partition="train",
     )
-test_dataset = NorecOneHot(
-    data_path=DATA_DIR + "test/", 
+test_dataset = Norec(
+    data_dir=DATA_DIR, 
     ignore_id=-1,
-    proportion=0.55,
+    partition="test",
+    proportion=1.,
     tokenizer=train_dataset.tokenizer,
     )
-dev_dataset = NorecOneHot(
-    data_path=DATA_DIR + "dev/", 
+dev_dataset = Norec(
+    data_dir=DATA_DIR, 
     ignore_id=-1,
-    proportion=0.55,
+    partition="dev",
+    proportion=1.,
     tokenizer=train_dataset.tokenizer,
     )
 
@@ -74,41 +91,27 @@ dev_loader = DataLoader(
 )
 logging.info("Datasets loaded.")
 
-if load_checkpoint:
-    try:
-        model = torch.load("/checkpoints/" + name + '.pt', map_location=torch.device(DEVICE))
-        logging.info("... from checkpoint/{}.pt".format(name))
-    except FileNotFoundError:
-        model = BertSimple(
-            device=DEVICE,
-            ignore_id=-1,
-            num_labels=5, 
-            lr=learning_rate,
-            tokenizer=train_dataset.tokenizer,
-            label_importance=label_importance,
-        )
-        logging.info("... from new instance.")
-
-else:
-    model = BertSimple(
-        device=DEVICE,
-        ignore_id=-1,
-        num_labels=5, 
-        lr=learning_rate,
-        tokenizer=train_dataset.tokenizer,
-        label_importance=label_importance,
-    )
-    logging.info("... from new instance.")
+logging.info("Loading new FgFlex model with the following parameters...")
+logging.info(parameters)
+model = FgFlex(
+    tokenizer=train_dataset.tokenizer,
+    **parameters,
+)
 
 logging.info('Fitting model...')
+print("Fitting model")
 model.fit(train_loader=train_loader, dev_loader=dev_loader, epochs=5)
 
 logging.info('Evaluating model...')
-easy_f1, hard_f1 = model.evaluate(dev_loader, verbose=True)
+absa, binary, hard, macro, proportional, span = model.evaluate(dev_loader, verbose=True)
 
-logging.info("Easy F1: {}".format(easy_f1))
-logging.info("Hard F1: {}".format(hard_f1))
+logging.info("  ABSA F1: {}".format(absa))
+logging.info("Binary F1: {}".format(binary))
+logging.info("  Hard F1: {}".format(hard))
+logging.info(" Macro F1: {}".format(Macro))
+logging.info(" Prop. F1: {}".format(proportional))
+logging.info("  Span F1: {}".format(span))
 
-logging.info("Saving model to {}".format("/checkpoints/" + name + '.pt'))
+logging.info("Saving model to {}".format("/checkpoints/" + parameters["name"] + '.pt'))
 torch.save(model, "/checkpoints/" + name + '.pt')
 
